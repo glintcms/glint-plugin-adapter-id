f72832ec598cca6f91675ecd6a4b10b7f787ada5
andi-neck <andi.neck@intesso.com>
2015-09-04 11:36:41 +0200

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# glint-plugin-adapter-id
+
+
+Auto id Attribute Adapter Plugin
+
+
+# install
+
+```bash
+npm install glint-plugin-adapter-id
+```
+
+# use
+
+> This Module is part of [glintcms](http://glintcms.com/).
+> Please see the [documentation](https://github.com/glintcms/glintcms) for more info.
+
+
+# test
+
+```bash
+npm test
+```
+
+# license
+
+MIT
+
+> sponsored by [intesso](http://intesso.com)
---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Module dependencies.
+ */
+var debug = require('debug')('glint:plugin-adapter-id');
+
+/**
+ *  Dates Adapter Plugin
+ *
+ *  adds the id attribute.
+ */
+module.exports = function (o) {
+  o = o || {};
+  o.id = 'id';
+
+  plugin.api = 'adapter-plugin';
+  plugin.name = 'id';
+  function plugin(adapter) {
+
+    adapter.on('pre-save', function () {
+      var args = [].slice.apply(arguments);
+      var len = args.length, pos = 3;
+      if (len <= pos) return debug('missing argument');
+      var obj = args[pos];
+      var id = args[pos - 1];
+      obj[o.id] = id;
+    });
+
+  }
+
+  return plugin;
+
+};
